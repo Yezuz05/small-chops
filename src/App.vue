3dff1bfb22665539c594bdc0925541407df923b8
yezuz05 <temitopeyussuph@gmail.com>
2019-11-11 02:24:27 +0100

    update: extras styles for side menu

--- src/App.vue
+++ src/App.vue
@@ -146,13 +146,13 @@ export default {
     z-index: 99;
     width: 300px;
     background-color: #fff;
-    padding: 10px 0 10px 20px;
+    padding: 45px 0 10px 20px;
     box-shadow: 0 0.5em 1em -0.125em rgba(10, 10, 10, 0.1),
       0 0px 0 1px rgba(10, 10, 10, 0.02);
     .close-box {
       display: flex;
       justify-content: flex-end;
-      padding-right: 20px;
+      padding-right: 25px;
       svg {
         cursor: pointer;
       }
